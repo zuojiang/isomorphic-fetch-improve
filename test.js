82db00ca9b0b4c6536a93fc4a6d26a561504cc13
zuojiang <zuojiang.wang@gmail.com>
2019-01-10 17:44:36 +0800

    :sparkles: UMD support

--- test.js
+++ test.js
@@ -1,18 +1,29 @@
 const http = require('http')
+const URL = require('url')
+const serve = require('serve-handler')
 const fetch = require('./index.js')
 
 const server = http.createServer((req, res) => {
-  // console.log(req.url);
-  switch (req.url) {
+  const {pathname} = URL.parse(req.url)
+  if (/\w+\.\w+/g.test(pathname)) {
+    serve(req, res, {
+      cleanUrls: false,
+    })
+    return
+  }
+  switch (pathname) {
     case '/delay':
       setTimeout(() => {
-        res.end()
+        res.end(new Date().toString())
       }, 5000)
       break
     case '/error':
       res.writeHead(500)
       res.end()
       break
+    default:
+      res.writeHead(404)
+      res.end()
   }
 })
 server.listen(3000)
